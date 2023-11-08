fa17054a9a8fa4a6201c32d4113c609b6cb70d7b
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-09 02:16:44 +0300

    Freebie lab

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,37 @@
 # !/usr/bin/env python3
 
 from sqlalchemy import create_engine
-
-from models import Company, Dev
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session  = Session()
     import ipdb; ipdb.set_trace()
+
+# engine = create_engine('sqlite:///freebies.db')
+
+
+# dev1 = Dev(name="Michael")
+# session.add(dev1)
+# session.commit()
+
+
+# company1 = Company("The star", 2022)
+# session.add(company1)
+# session.commit()
+
+# freebie1 = Freebie(item_name="hat",value=10,dev_id=dev1.id,company_id=company1.id)
+# session.add(freebie1)
+# session.commit()
+
+# Script goes here!
+# print(dev1)
+# print(company1)
+# print(dev1.freebies)
+# print(dev1.companies)
+# print(company1.freebies)
+# print(company1.devs)
+# print(freebie1.dev)
+# print(freebie1.company)
--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,20 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table, func
+from sqlalchemy.orm import relationship, backref, declarative_base
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
+
+dev_company = Table(
+    'dev_companies',
+    Base.metadata,
+    Column('dev_id', ForeignKey('devs.id'), primary_key=True),
+    Column('company_id', ForeignKey('companies.id'), primary_key=True),
+    extend_existing=True
+)
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -15,6 +22,18 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
+
+    freebies = relationship('Freebie', back_populates='company')
+    devs = relationship('Dev', secondary=dev_company, back_populates='companies')
+
+    def give_freebie(self,dev, item_name, value):
+        freebie = Freebie(item_name=item_name, value=value, dev_id=dev.id,company_id=self.id)
+        return freebie
+    
+    def oldest_company(self):
+        company_years = []
+        company_years.append(self.founding_year)
+        pass
 
     def __repr__(self):
         return f'<Company {self.name}>'
@@ -25,6 +44,9 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', back_populates='dev')
+    companies = relationship('Company', secondary=dev_company, back_populates='devs')
+
     def __repr__(self):
         return f'<Dev {self.name}>'
 
@@ -34,5 +56,14 @@ class Freebie(Base):
     id = Column(Integer(), primary_key=True)
     item_name = Column(String())
     value = Column(Integer())
-    dev_id = Column(Integer, ForeignKey('devs.id'))
-    company_id = Column(Integer, ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+
+    company = relationship('Company', back_populates='freebies')
+    dev = relationship('Dev', back_populates='freebies')
+
+    def print_details(self):
+        return f'{self.dev} owns a {self.item_name} from {self.company}'
+
+    def __repr__(self):
+        return f'<Dev item_name={self.item_name}, value={self.value}>'
